05530b80c26d665d2e7dc2b1ec391ab97bafaae6
Ben Tillman <ben.tillman@gmail.com>
2013-11-08 10:07:27 -0800

    adds filter to uniqly sample from an array

--- lib/locomotive/liquid_extras/filters/sample.rb
+++ lib/locomotive/liquid_extras/filters/sample.rb
@@ -12,6 +12,11 @@ module Locomotive
           end
         end
 
+        def sample!(input)
+          input = input.all if input.respond_to?(:all) # Content type collection
+          input.delete input.sample
+        end
+
       end
 
       ::Liquid::Template.register_filter(Sample)
--- spec/locomotive/liquid_extras/filters/sample_spec.rb
+++ spec/locomotive/liquid_extras/filters/sample_spec.rb
@@ -4,9 +4,19 @@ describe Locomotive::LiquidExtras::Filters::Sample do
 
   include Locomotive::LiquidExtras::Filters::Sample
 
-  it 'return a sample of an array' do
-    array = ["Foo", "Bar", "Locomotive"]
-    (array - sample(array, 1)).size.should eql 2
+  describe '#sample' do
+    it 'return a sample of an array' do
+      array = ["Foo", "Bar", "Locomotive"]
+      (array - sample(array, 1)).size.should eql 2
+    end
   end
 
+  describe '#sample!' do
+    it 'return a unique sample of an array' do
+      array = ["Foo", "Bar", "Locomotive"]
+      set = Set.new()
+      3.times { set << sample!(array) }
+      set.size.should eql 3
+    end
+  end
 end
